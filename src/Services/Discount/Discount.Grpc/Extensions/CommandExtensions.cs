834219a755948ab024adc05c169fde344e76735b
scaltas <scaltas@gmail.com>
2021-12-29 18:34:01 +0300

    Discount Grpc with PostgreSQL

---
+++ src/Services/Discount/Discount.Grpc/Extensions/CommandExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+using Npgsql;
+
+namespace Discount.Grpc.Extensions
+{
+    public static class CommandExtensions
+    {
+        public static void ExecuteNonQuery(this NpgsqlCommand command, string commandText)
+        {
+            command.CommandText = commandText;
+            command.ExecuteNonQuery();
+        }
+    }
+}
