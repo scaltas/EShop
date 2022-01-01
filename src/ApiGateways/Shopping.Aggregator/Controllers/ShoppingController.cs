0c2969a26b4e11a4f40cde5d32ffe7fab2503d2c
scaltas <scaltas@gmail.com>
2022-01-02 00:53:31 +0300

    Shopping Aggregator

---
+++ src/ApiGateways/Shopping.Aggregator/Controllers/ShoppingController.cs
@@ -0,0 +1,52 @@
+﻿using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Shopping.Aggregator.Models;
+using Shopping.Aggregator.Services;
+
+namespace Shopping.Aggregator.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class ShoppingController : ControllerBase
+    {
+        private readonly ICatalogService _catalogService;
+        private readonly IBasketService _basketService;
+        private readonly IOrderService _orderService;
+        public ShoppingController(ICatalogService catalogService, IBasketService basketService, IOrderService orderService)
+        {
+            _catalogService = catalogService;
+            _basketService = basketService;
+            _orderService = orderService;
+        }
+
+        [HttpGet("{username}")]
+        [ProducesResponseType(typeof(ShoppingModel), (int)HttpStatusCode.OK)]
+        public async Task<IActionResult> GetShopping(string username)
+        {
+            var basket = await _basketService.GetBasket(username);
+            foreach (var item in basket.Items)
+            {
+                var product = await _catalogService.GetCatalog(item.ProductId);
+
+                item.ProductName = product.Name;
+                item.Description = product.Description;
+                item.Category = product.Category;
+                item.Summary = product.Summary;
+                item.ImageFile = product.ImageFile;
+            }
+
+            var orders = await _orderService.GetOrdersByUserName(username);
+
+            var shoppingModel = new ShoppingModel()
+            {
+                UserName = username,
+                Orders = orders,
+                BasketWithProducts = basket
+            };
+
+            return Ok(shoppingModel);
+        }
+    }
+}
